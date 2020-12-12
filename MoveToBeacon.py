61a7706fad7cd2ba38081873152af93aba6a023a
Luker <luker.plus@gmail.com>
2020-12-12 23:29:10 +0100

    Initial code to be adapted

--- MoveToBeacon.py
+++ MoveToBeacon.py
@@ -28,3 +28,24 @@ class MoveToBeacon(base_agent.BaseAgent):
             return FUNCTIONS.select_army(False)
         coords_to_go_to = decide_coords(obs.observation.feature_screen.player_relative)
         return FUNCTIONS.Move_screen(False, coords_to_go_to)
+
+
+if __name__ == "__main__":
+    agent1 = SmartAgent()
+    agent2 = RandomAgent()
+    try:
+        with sc2_env.SC2Env(
+                map_name="Simple64",
+                players=[sc2_env.Agent(sc2_env.Race.terran),
+                         sc2_env.Agent(sc2_env.Race.terran)],
+                agent_interface_format=features.AgentInterfaceFormat(
+                    action_space=actions.ActionSpace.RAW,
+                    use_raw_units=True,
+                    raw_resolution=64,
+                ),
+                step_mul=48,
+                disable_fog=True,
+        ) as env:
+            run_loop.run_loop([agent1, agent2], env, max_episodes=1000)
+    except KeyboardInterrupt:
+        pass
